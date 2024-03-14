4ae46ff56f366e6488f8a1a2c9a670768b353a5e
Alphons Jaimon <jaimonalphons@gmail.com>
2024-03-14 20:00:11 +0530

    multi container

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// Function to dynamically generate PM2 app configurations
+const generateAppConfigs = () => {
+  const envFiles = fs.readdirSync('./').filter(file => file.endsWith('.run.env'));
+  return envFiles.map(file => {
+    const projectName = file.split('.').slice(0, -2).join('.');
+    return {
+      name: `text-embedding-inference-${projectName}`,
+      script: 'docker-compose',
+      args: `-p ${projectName} --env-file ${file} up`,
+      watch: false,
+      interpreter: 'none', // Important for running shell commands
+      max_memory_restart: '1000M',
+      autorestart: true,
+      max_restarts: 10,
+    };
+  });
+};
+
+module.exports = {
+  apps: generateAppConfigs(),
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
 # huggingface/text-embeddings-inference
-
-make copies of the env file to start different instances of the same `docker-compose --env-file project1.env up`
